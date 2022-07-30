b112db009a99eae2d25c93640a70bf9c044c4777
André Franco <105262693+andrefranco001@users.noreply.github.com>
2022-07-30 11:25:07 -0300

    Delete Java directory

--- Java/Logica/src/checkpoint_13_06_2022/entidades/Nivel.java
+++
@@ -1,7 +0,0 @@
-package checkpoint_13_06_2022.entidades;
-
-public enum Nivel {
-    JUNIOR,
-    PLENO,
-    SENIOR;
-}
--- Java/Logica/src/sequencial/Main.java
+++
@@ -1,9 +0,0 @@
-package sequencial;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.print("Seja bem-vindo(a) a TI Academy Brasil.\n");
-        System.out.println("Este Código estará na linha debaixo");
-        System.out.println(256*3);
-    }
-}
