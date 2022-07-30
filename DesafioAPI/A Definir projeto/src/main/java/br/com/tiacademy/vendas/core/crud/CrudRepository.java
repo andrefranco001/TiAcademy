5f99a279c0d32c98e5a524e09ee1c0357725fa8c
André Franco <105262693+andrefranco001@users.noreply.github.com>
2022-07-30 11:44:07 -0300

    Delete DesafioAPI/A Definir projeto directory

--- DesafioAPI/A Definir projeto/src/main/java/br/com/tiacademy/vendas/core/crud/CrudRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.tiacademy.vendas.core.crud;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.NoRepositoryBean;
-import org.springframework.stereotype.Repository;
-
-@NoRepositoryBean
-public interface CrudRepository<T, ID> extends JpaRepository<T, ID> {}
